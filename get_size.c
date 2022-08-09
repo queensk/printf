2e5aed575ec083d51e7574bf630edcfc33882c2f
queensk <queenskisivuli@gmail.com>
2022-08-09 04:49:37 -0700

    fixed bety style

--- get_size.c
+++ get_size.c
@@ -5,6 +5,7 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
+ * Description: The get_size calculats the size of argument
  * Return: Precision.
  */
 int get_size(const char *format, int *i)
