aaab949b2d3141c4b5d817a4d0853afefd56c9d0
queensk <queenskisivuli@gmail.com>
2022-08-09 02:14:37 -0700

    fixed the betty style

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ *
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -56,6 +57,10 @@ int _printf(const char *format, ...)
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
+ *
+ * Description: The if statment checks the buffer content and prints it.
+ * The printing is only when it exists.
+ * Return: void
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
